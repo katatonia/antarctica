1da8f40235bc2931dcd96ba988cf8fbcf1e9d5a1
katatonia <pmilekhina@gmail.com>
2022-12-05 13:29:15 +0400

    Добавляет лолгику меню

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,8 +1,11 @@
 import {createElementsArrow} from '../utils/util';
 
+const body = document.body;
 const burger = document.querySelector('.menu__toggle--burger');
 const iconClose = document.querySelector('.menu__toggle--icon-close');
+const menuItemArrow = document.querySelectorAll('.menu__list-item');
 const header = document.querySelector('.header');
+const headerOverlay = document.querySelector('.header__overlay');
 const headerMenu = document.querySelector('.header__menu');
 const headerLogo = document.querySelector('.header__logo');
 const menuNav = document.querySelector('.menu__nav');
@@ -16,34 +19,66 @@ const removeClass = (arr) => {
   });
 };
 
+const showIconClose = () => {
+  burger.style.display = 'none';
+  iconClose.style.display = 'block';
+  header.classList.add('opened');
+  headerMenu.classList.add('opened');
+  menuNav.classList.add('opened');
+  headerLogo.classList.add('opened');
+  mainContainer.style.visibility = 'hidden';
+  intro.style.marginTop = '112px';
+  headerOverlay.classList.add('opened');
+};
+
+const showBurger = () => {
+  burger.style.display = 'block';
+  iconClose.style.display = 'none';
+  header.classList.remove('opened');
+  headerMenu.classList.remove('opened');
+  menuNav.classList.remove('opened');
+  headerLogo.classList.remove('opened');
+  mainContainer.style.visibility = 'visible';
+  intro.style.marginTop = '0';
+  headerOverlay.classList.remove('opened');
+};
+
+const onLinkClick = () => {
+  createElementsArrow(menuItemArrow).forEach((e) => {
+    e.addEventListener('click', () => {
+      showBurger();
+    });
+  });
+};
+
+const onOverlayClick = () => {
+  headerOverlay.addEventListener('click', () => {
+    showBurger();
+  });
+};
+
 const showMenu = () => {
   if (burger && header && headerMenu && menuNav && headerLogo && mainContainer && iconClose && intro) {
     removeClass(createElementsArrow(noJsArrow));
 
     burger.addEventListener('click', () => {
-      burger.style.display = 'none';
-      iconClose.style.display = 'block';
-      header.classList.add('opened');
-      headerMenu.classList.add('opened');
-      menuNav.classList.add('opened');
-      headerLogo.classList.add('opened');
-      mainContainer.style.visibility = 'hidden';
-      intro.style.marginTop = '112px';
+      showIconClose();
     });
   }
 
   if (iconClose && header && headerMenu && menuNav && burger && headerLogo && mainContainer && iconClose && intro) {
+    if (headerOverlay.classList.contains('opened')) {
+      body.style.overflow = 'hidden';
+    }
+
     iconClose.addEventListener('click', () => {
-      burger.style.display = 'block';
-      iconClose.style.display = 'none';
-      header.classList.remove('opened');
-      headerMenu.classList.remove('opened');
-      menuNav.classList.remove('opened');
-      headerLogo.classList.remove('opened');
-      mainContainer.style.visibility = 'visible';
-      intro.style.marginTop = '0';
+      showBurger();
     });
   }
+
+  onLinkClick();
+  onOverlayClick();
+
 };
 
 export {showMenu};
